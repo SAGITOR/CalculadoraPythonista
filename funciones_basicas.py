2abf7cf843eb20f58809de57a88acc3d88c5a3c6
SAGITOR <montanius12@gmail.com>
2023-11-05 15:25:14 -0300

    Se crea e integra la funcion para la operacion de resta entre dos numeros

--- funciones_basicas.py
+++ funciones_basicas.py
@@ -11,20 +11,37 @@ def suma(sumando_1, sumando_2):
     """
     return sumando_1 + sumando_2
 
+def resta(minuendo, sustraendo):
+    """
+    Esta función realiza una resta de dos números.
+
+    Args:
+    minuendo (float): El número del que se restará.
+    sustraendo (float): El número que se restará.
+
+    Returns:
+    float: La resta de minuendo menos sustraendo.
+    """
+    return minuendo - sustraendo
+
 def main():
     resultado = 0
     print("Ingrese el número de la opcion de la operacion basica que desea realizar: ")
-    operacion = int(input("1 = suma: "))
+    operacion = int(input("1 = suma, 2 = resta: "))
 
     
-    if (operacion < 1 or operacion > 1):
+    if (operacion < 1 or operacion > 2):
         return print("Opción no válida")
     else:
         if (operacion == 1): 
             sumando_1 = float(input("sumando 1: "))
             sumando_2 = float(input("sumando 2: "))
             resultado = suma(sumando_1, sumando_2)
-        
+        elif (operacion == 2):
+            minuendo = float(input("minuendo: "))
+            sustraendo = float(input("sustraendo: "))
+            resultado = resta(minuendo, sustraendo)
+
         print(f"El resultado de la operacion es: {resultado}")
 
 main()
